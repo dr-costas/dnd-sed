adc7b9e40dccc3b323c964be4b02278a6811da3d
drossos <kostas.drosos@icloud.com>
2020-02-03 14:59:01 +0200

    Init of the data handlers.

---
+++ data_feeders/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ._data_loader_functions import get_tut_sed_data_loader
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['get_tut_sed_data_loader']
+
+
+# EOF
