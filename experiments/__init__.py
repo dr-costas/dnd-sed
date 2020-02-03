9bc6f5ab0141f170a8ac616f7a6e15ec942de668
drossos <kostas.drosos@icloud.com>
2020-02-03 14:53:03 +0200

    Init of the processes.

---
+++ experiments/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from experiments import experiment_process
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['experiment_process']
+
+
+# EOF
