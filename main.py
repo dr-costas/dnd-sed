2261a234c28471e7f2251511ce37715112c893ba
drossos <kostas.drosos@icloud.com>
2020-03-09 10:01:54 +0200

    Main entry point of the project.

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from experiments.experiment_process import do_process
+from tools.file_io import load_settings_file
+from tools.various import get_argument_parser, init_loggers
+
+__author__ = 'Konstantinos Drossos'
+__docformat__ = 'reStructuredText'
+__all__ = []
+
+
+def main():
+    args = get_argument_parser().parse_args()
+    model_to_use = args.model
+    config_file = args.config_file
+    file_ext = args.file_ext
+    settings_dir = args.file_dir
+    verbose = args.verbose
+
+    settings = load_settings_file(
+        file_name=Path(f'{config_file}{file_ext}'),
+        settings_dir=Path(settings_dir))
+
+    init_loggers(verbose=verbose)
+
+    do_process(
+        settings_path=None,
+        settings=settings,
+        model_to_use=model_to_use)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
